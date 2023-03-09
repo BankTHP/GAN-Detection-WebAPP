589016bd16d189fbfce9a37c352cabca31aa5c21
BankTHP <bang.225588z@gmail.com>
2023-03-09 14:50:18 +0700

    Bank push

--- app.py
+++ app.py
@@ -70,13 +70,24 @@ def detect_upload_image():
         flash('Allowed image types are - png, jpg, jpeg, gif')
         return redirect(request.url)
 
-@app.route('/output',methods=['GET','POST'])
+@app.route('/detect',methods=['GET','POST'])
 def generate():
-    if request.method == 'POST' : 
-        title = request.form["title"] 
-        print(title)
-        return render_template('output.html')
-    return render_template('output.html')
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        return render_template('detect.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+    
 
 @app.route('/display/<filename>')
 def display_image(filename):
